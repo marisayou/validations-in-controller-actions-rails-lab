5bce2acd1271f2121be035b776270788a3cfa7d2
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-01 16:22:56 -0800

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    has_many :posts
+
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :content, length: { minimum: 100 }
 end
